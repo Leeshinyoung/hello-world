886022d89724abb147f9b3c75c0218aa6087f84c
imun <imun@10.11.13.5>
2018-05-18 11:00:03 +0900

    자기소개서 만들기

---
+++ src/VariablesExercise.java
@@ -0,0 +1,26 @@
+public class VariablesExercise {
+	public static void main(String[] args) {
+		//변수 정의(자료형 잘 선정해서)
+		String name;
+		String year;
+		String month;
+		String day;
+		Boolean married;
+		
+		//값대입
+		name = "코드잇";
+		year = "2015";
+		month = "7";
+		day = "20";
+		married = false;
+		
+		//자기소개
+		System.out.print("이름: ");
+		System.out.println(name);
+		System.out.print("생년월일: ");
+		System.out.print(year);
+		System.out.println(month);
+		System.out.print("결혼여부: ");
+		System.out.println(married);
+				}
+}
